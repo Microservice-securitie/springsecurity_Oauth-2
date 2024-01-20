4984bbce8e0dc2fbf4ba17886423fbd821bc3a8b
othmanetozy <tozyothmane123@gmail.com>
2024-01-20 14:13:31 +0100

    add config properties

--- customer-front-tymeleaf-app/src/main/java/emsi/app/customerfronttymeleafapp/CustomerFrontTymeleafAppApplication.java
+++ customer-front-tymeleaf-app/src/main/java/emsi/app/customerfronttymeleafapp/CustomerFrontTymeleafAppApplication.java
@@ -1,7 +1,11 @@
 package emsi.app.customerfronttymeleafapp;
 
+import emsi.app.customerfronttymeleafapp.entity.Customer;
+import emsi.app.customerfronttymeleafapp.repo.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerFrontTymeleafAppApplication {
@@ -10,4 +14,12 @@ public class CustomerFrontTymeleafAppApplication {
         SpringApplication.run(CustomerFrontTymeleafAppApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
+        return args -> {
+            customerRepository.save(Customer.builder().name("Othmane").email("otozy@gmail.com").build());
+            customerRepository.save(Customer.builder().name("mohamed").email("elmohamed@gmail.com").build());
+            customerRepository.save(Customer.builder().name("hafsae").email("shafsae@gmail.com").build());
+        };
+    }
 }
