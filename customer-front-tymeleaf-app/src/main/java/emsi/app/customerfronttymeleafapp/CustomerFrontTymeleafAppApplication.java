bf8b0adfbc6f464b541b12644dedb69c2513f066
othmanetozy <tozyothmane123@gmail.com>
2024-01-20 11:58:44 +0100

    first commit

---
+++ customer-front-tymeleaf-app/src/main/java/emsi/app/customerfronttymeleafapp/CustomerFrontTymeleafAppApplication.java
@@ -0,0 +1,13 @@
+package emsi.app.customerfronttymeleafapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerFrontTymeleafAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerFrontTymeleafAppApplication.class, args);
+    }
+
+}
---
+++ customer-front-tymeleaf-app/src/test/java/emsi/app/customerfronttymeleafapp/CustomerFrontTymeleafAppApplicationTests.java
@@ -0,0 +1,13 @@
+package emsi.app.customerfronttymeleafapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerFrontTymeleafAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
