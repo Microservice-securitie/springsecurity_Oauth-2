de4299f3deadffaa77b73ef0e2a1c8eea61a4b85
othmanetozy <tozyothmane123@gmail.com>
2024-01-20 22:49:31 +0100

    adding securityConfig

---
+++ customer-front-tymeleaf-app/src/main/java/emsi/app/customerfronttymeleafapp/config/Securityconfig.java
@@ -0,0 +1,23 @@
+package emsi.app.customerfronttymeleafapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Securityconfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(ar->ar.requestMatchers("/","/webjars/**,", "/h2-console/**").permitAll())
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
