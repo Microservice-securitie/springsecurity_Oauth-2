361b72a512f1049f717227a213cfcff53a0932f5
othmanetozy <tozyothmane123@gmail.com>
2024-01-23 15:44:30 +0100

    return format json

--- customer-front-tymeleaf-app/src/main/java/emsi/app/customerfronttymeleafapp/controller/CustomerController.java
+++ customer-front-tymeleaf-app/src/main/java/emsi/app/customerfronttymeleafapp/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -31,6 +32,7 @@ public class CustomerController {
     }
 
     @GetMapping("/auth")
+    @ResponseBody
     public Authentication authentication(Authentication authentication){
         return authentication;
     }
