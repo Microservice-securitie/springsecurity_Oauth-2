2d007a4cfc91d8533e3853a4799422e5653f59b1
othmanetozy <tozyothmane123@gmail.com>
2024-01-23 15:41:36 +0100

    changes clientId and client-secret for desktop application

--- customer-front-tymeleaf-app/src/main/java/emsi/app/customerfronttymeleafapp/config/Securityconfig.java
+++ customer-front-tymeleaf-app/src/main/java/emsi/app/customerfronttymeleafapp/config/Securityconfig.java
@@ -18,6 +18,9 @@ public class Securityconfig {
                 .authorizeHttpRequests(ar->ar.requestMatchers("/","/webjars/**", "/h2-console/**").permitAll())
                 .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
                 .oauth2Login(Customizer.withDefaults())
+                .logout((logout)->logout
+                        .logoutSuccessUrl("/").permitAll()
+                        .deleteCookies("JSESSIONID"))
                 .build();
     }
 }
