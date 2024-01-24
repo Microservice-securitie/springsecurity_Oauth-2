ca469e5f20591c5dffc333540993c6a7e5dc908a
othmanetozy <tozyothmane123@gmail.com>
2024-01-24 12:43:21 +0100

    configure OIDC Logout success Handling with Post-Logout redirect URI

--- customer-front-tymeleaf-app/src/main/java/emsi/app/customerfronttymeleafapp/config/Securityconfig.java
+++ customer-front-tymeleaf-app/src/main/java/emsi/app/customerfronttymeleafapp/config/Securityconfig.java
@@ -5,11 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class Securityconfig {
+
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    public Securityconfig(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -19,8 +27,17 @@ public class Securityconfig {
                 .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
                 .oauth2Login(Customizer.withDefaults())
                 .logout((logout)->logout
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
                         .logoutSuccessUrl("/").permitAll()
+                        .clearAuthentication(true)
                         .deleteCookies("JSESSIONID"))
                 .build();
     }
+    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        final OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}?logoutsuccess=true");
+        return oidcLogoutSuccessHandler;
+    }
+
 }
