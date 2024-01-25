e92198e555e8d3dbc206ff54c28684bd4141a147
othmanetozy <tozyothmane123@gmail.com>
2024-01-25 23:14:36 +0100

    show table in products

--- customer-front-angular-app/src/app/Products/products.component.ts
+++ customer-front-angular-app/src/app/Products/products.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
+
+
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  products : any;
+  constructor(private http : HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.get("http://localhost/8888/products")
+      .subscribe({
+        next : data => {
+          this.products=data;
+        },
+        error : err => {
+          console.log(err)
+        }
+      })
+  }
+
 
 }
--- customer-front-angular-app/src/app/app.module.ts
+++ customer-front-angular-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomersComponent } from './Customers/customers.component';
 import { ProductsComponent } from './Products/products.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProductsComponent } from './Products/products.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
