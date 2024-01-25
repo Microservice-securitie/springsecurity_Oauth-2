6ce7818c04476428009af87dc37cb5ebf1e476dd
othmanetozy <tozyothmane123@gmail.com>
2024-01-25 11:19:08 +0100

    adding security config service inventory

---
+++ inventory-service/src/main/java/emsi/app/inventoryservice/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package emsi.app.inventoryservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .oauth2ResourceServer(o2->o2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
