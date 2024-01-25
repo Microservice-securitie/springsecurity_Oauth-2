04ce61b8a85864fc0c6ce49323a99e0cd761f52b
othmanetozy <tozyothmane123@gmail.com>
2024-01-25 19:52:53 +0100

    adding products and customers card with heading

--- customer-front-angular-app/src/app/app-routing.module.ts
+++ customer-front-angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CustomersComponent} from "./Customers/customers.component";
+import {ProductsComponent} from "./Products/products.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"customers" , component : CustomersComponent},
+  {path: "products", component : ProductsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- customer-front-angular-app/src/app/app.module.ts
+++ customer-front-angular-app/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomersComponent } from './customers/customers.component';
-import { ProductsComponent } from './products/products.component';
+import { CustomersComponent } from './Customers/customers.component';
+import { ProductsComponent } from './Products/products.component';
 
 @NgModule({
   declarations: [
