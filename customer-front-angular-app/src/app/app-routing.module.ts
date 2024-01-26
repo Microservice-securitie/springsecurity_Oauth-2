0be3259e13616cbac19d33a986ecd8d3df24bf09
othmanetozy <tozyothmane123@gmail.com>
2024-01-26 15:06:31 +0100

    Creating and implement Keycloak route Guard for Authentication and Authorization"

--- customer-front-angular-app/src/app/app-routing.module.ts
+++ customer-front-angular-app/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CustomersComponent} from "./Customers/customers.component";
 import {ProductsComponent} from "./Products/products.component";
+import {AuthGuard} from "./guards/auth.guard";
 
 const routes: Routes = [
   {path:"customers" , component : CustomersComponent},
-  {path: "products", component : ProductsComponent}
+  {path: "products", component : ProductsComponent , canActivate:[AuthGuard] , data : {roles:["ADMIN"]}}
 ];
 
 @NgModule({
