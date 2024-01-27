2742299576050952929751468847684045deb3ed
othmanetozy <tozyothmane123@gmail.com>
2024-01-27 16:28:39 +0100

    Authorized products for role ADMIN and disable for users

--- customer-front-angular-app/src/app/app.component.ts
+++ customer-front-angular-app/src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit{
       });
     }
   }
-  constructor(private keycloak : KeycloakService) {
+  constructor(public keycloak : KeycloakService) {
 
   }
   async login(){
